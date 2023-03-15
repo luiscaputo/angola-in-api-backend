016dec88588a9313ade660acb59e24dc0b5c47aa
luiscaputo <luiscaputo15@gmail.com>
2023-03-15 18:33:54 +0100

    feat: Ajusted

--- src/modules/personalIdentification/IdentificationController.ts
+++ src/modules/personalIdentification/IdentificationController.ts
@@ -3,12 +3,12 @@ import { Request, Response } from 'express';
 import IdentificationUseCase from './IdentificationUseCase';
 
 export default class IdentificationController {
-  async handle(req: Request<{ identificationNumber: string }>, res: Response) {
-    const { identificationNumber } = req.params;
+  async handle(req: Request<{ bi: string }>, res: Response) {
+    const { bi } = req.params;
     const useCase = new IdentificationUseCase();
 
     const result = await useCase.execute({
-      identificationNumver: identificationNumber,
+      identificationNumver: bi,
     });
 
     return res.status(200).json({
