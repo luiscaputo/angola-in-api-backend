fd282412709ede167f573da883b6d6b73cd555da
luiscaputo <luiscaputo15@gmail.com>
2023-03-15 18:34:12 +0100

    feat: Ajusted

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,5 @@
+import companyValidationRoutes from '~/modules//companyIdentification/company.routes';
+import locationRoutes from '~/modules/locations/locations.routes';
 import identificationRoutes from '~/modules/personalIdentification/identification.routes';
 import userRoutes from '~/modules/users/http/routes';
 import { Router } from 'express';
@@ -9,5 +11,7 @@ const routes = Router();
 routes.use(baseRoute);
 routes.use(userRoutes);
 routes.use(identificationRoutes);
+routes.use(companyValidationRoutes);
+routes.use(locationRoutes);
 
 export default routes;
