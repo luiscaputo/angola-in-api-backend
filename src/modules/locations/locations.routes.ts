752dfa32b6bac394ee93b10a04b9e2243a351dec
luiscaputo <luiscaputo15@gmail.com>
2023-03-20 11:16:44 +0100

    feat: Locations Routes

--- src/modules/locations/locations.routes.ts
+++ src/modules/locations/locations.routes.ts
@@ -4,6 +4,6 @@ import LocationsControllers from './LocationController';
 
 const router = Router();
 
-router.get('/locations/provinces', new LocationsControllers().handleProvinces);
+router.get('/locations/provinces', new LocationsControllers().handle);
 
 export default router;
