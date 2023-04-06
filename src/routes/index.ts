d447fe7198949a6984336714d410a287a30ed624
luiscaputo <luiscaputo15@gmail.com>
2023-04-06 14:56:33 +0100

    :books: Docs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import companyValidationRoutes from '~/modules//companyIdentification/company.ro
 import locationRoutes from '~/modules/locations/locations.routes';
 import identificationRoutes from '~/modules/personalIdentification/identification.routes';
 import userRoutes from '~/modules/users/http/routes';
+import phoneNumberRoutes from '~/modules/phoneNumbers/phoneNumber.routes';
+import marketsRoutes from '~/modules/markets/markets.routes';
 import { Router } from 'express';
 
 import baseRoute from './base.routes';
@@ -13,5 +15,7 @@ routes.use(userRoutes);
 routes.use(identificationRoutes);
 routes.use(companyValidationRoutes);
 routes.use(locationRoutes);
+routes.use(phoneNumberRoutes);
+routes.use(marketsRoutes);
 
 export default routes;
